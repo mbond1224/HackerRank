9b3520b9c10bf58ba9b064f8de05ee9f8a8c0c17
Mary Bond <mbond1224@users.noreply.github.com>
2018-02-10 11:25:55 -0800

    Create DatesTest.js
    Solution to the problem listed at 
    https://www.hackerrank.com/challenges/js10-date/problem

---
+++ 10 Days of Javascript/DatesTest.js
@@ -0,0 +1,45 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+//Main Code Starts
+
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    let dayName;
+    // Write your code here
+    let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let dt=new Date(dateString);
+    let dtidx=dt.getDay()
+    dayName=days[dtidx];
+    return dayName;
+}
+//Main Code Ends
+function main() {
+    const d = +(readLine());
+    
+    for (let i = 0; i < d; i++) {
+        const date = readLine();
+        
+        console.log(getDayName(date));
+    }
+}
