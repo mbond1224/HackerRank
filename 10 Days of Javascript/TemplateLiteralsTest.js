28a6ca4908ef9894ce04265fed789c8a6c53731e
Mary Bond <mbond1224@users.noreply.github.com>
2018-02-10 09:25:20 -0800

    Update TemplateLiteralsTest.js
    put the main Code and the remaining provided Code. 
    for the problem 
    https://www.hackerrank.com/challenges/js10-template-literals/problem

--- 10 Days of Javascript/TemplateLiteralsTest.js
+++ 10 Days of Javascript/TemplateLiteralsTest.js
@@ -22,3 +22,38 @@ function readLine() {
     return inputString[currentLine++];
 }
 //Main Code Starts
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ * 
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function sides(literals, ...expressions) {
+    let A=expressions[0];
+    let P=expressions[1];
+    let op=[];
+    let s1=(P+Math.sqrt(P*P-16*A))/4;
+    let s2=(P-Math.sqrt(P*P-16*A))/4;
+    op.push(s1);
+    op.push(s2);
+    op.sort(function(a,b){
+        return a-b;
+    })
+    return op;
+    
+}
+//Main Code Ends
+function main() {
+    let s1 = +(readLine());
+    let s2 = +(readLine());
+    
+    [s1, s2] = [s1, s2].sort();
+    
+    const [x, y] = sides`The area is: ${s1 * s2}.\nThe perimeter is: ${2 * (s1 + s2)}.`;
+    
+    console.log((s1 === x) ? s1 : -1);
+    console.log((s2 === y) ? s2 : -1);
+}
