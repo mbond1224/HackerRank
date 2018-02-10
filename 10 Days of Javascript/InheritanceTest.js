e63ddce2e844d6e652e760d3a5dc7a4c49f32278
Mary Bond <mbond1224@users.noreply.github.com>
2018-02-10 09:22:20 -0800

    Update Inheritance Test.js
    put the remaining the given code. Original Problem at 
    https://www.hackerrank.com/challenges/js10-inheritance/problem

--- 10 Days of Javascript/InheritanceTest.js
+++ 10 Days of Javascript/InheritanceTest.js
@@ -20,3 +20,13 @@ class Square extends Rectangle{
     }
 }
 //Main Code Ends
+if (JSON.stringify(Object.getOwnPropertyNames(Square.prototype)) === JSON.stringify([ 'constructor' ])) {
+    const rec = new Rectangle(3, 4);
+    const sqr = new Square(3);
+    
+    console.log(rec.area());
+    console.log(sqr.area());
+} else {
+    console.log(-1);
+    console.log(-1);
+}
