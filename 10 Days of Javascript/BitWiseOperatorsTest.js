fcc04062685b23a665090856a6934a9abcef5bc0
Mary Bond <mbond1224@users.noreply.github.com>
2018-02-10 11:02:49 -0800

    Create BitWiseOperatorsTest.js
    Solution for  problem listed at 
    https://www.hackerrank.com/challenges/js10-bitwise/problem

---
+++ 10 Days of Javascript/BitWiseOperatorsTest.js
@@ -0,0 +1,52 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+//Main Code Starts
+/*
+*	Return the largest value of any a & b < k in S (where a < b).
+* 	
+*	n: Set S is a sequence of distinct integers from 1 to n (i.e., {1, 2, ..., n}).
+*	k: An integer.
+*/
+function getMaxLessThanK(n, k) {
+    let max=-1;
+    let num;
+    for(let i=1;i<n;i++){
+        for(let j=i+1;j<=n;j++){
+            num=i&j;
+            if (num<k && num >max)
+                max=num;
+        }
+    }
+    return max;
+}
+//Main Code Ends
+function main() {
+    const q = +(readLine());
+    
+    for (let i = 0; i < q; i++) {
+        const [n, k] = readLine().split(' ').map(Number);
+        
+        console.log(getMaxLessThanK(n, k));
+    }
+}
