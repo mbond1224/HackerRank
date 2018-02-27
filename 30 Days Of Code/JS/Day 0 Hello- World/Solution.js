cfb14c2be57c58e062fcff448e4316318f13e684
Mary Bond <mbond1224@users.noreply.github.com>
2018-02-26 22:15:07 -0800

    Create Solution.js
    Solution for the problem listed at   https://www.hackerrank.com/challenges/30-hello-world/problem

---
+++ 30 Days Of Code/JS/Day 0 Hello- World/Solution.js
@@ -0,0 +1,19 @@
+//Main Code Starts
+function processData(inputString) {
+    // This line of code prints the first line of output
+    console.log("Hello, World.");
+    
+    // Write the second line of output that prints the contents of 'inputString' here.
+} 
+//Main Code Ends
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
