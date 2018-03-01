edb22b94bfdc5a9ba1ac0b316c36b4323ab8dd50
Mary Bond <mbond1224@users.noreply.github.com>
2018-03-01 13:26:53 -0800

    Update Solution.js
    Solution to the problem at https://www.hackerrank.com/challenges/30-linked-list/problem

--- 30 Days Of Code/JS/15 Linked List/Solution.js
+++ 30 Days Of Code/JS/15 Linked List/Solution.js
@@ -1,1 +1,16 @@
 
+	this.insert=function(head,data){
+      let node ={data:data,next:null};
+        let start=head;
+         if(!head){
+               return node;
+         }
+         while(start.next)
+         {
+             start= start.next;
+         }
+      
+        start.next=node;
+        return head;
+    
+    };
