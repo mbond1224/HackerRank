851c0b76521170d23667cc30c1d414b55141516d
Mary Bond <mbond1224@users.noreply.github.com>
2018-03-01 13:23:14 -0800

    Create Solution.js
    Solution to the problem listed at https://www.hackerrank.com/challenges/30-abstract-classes/problem

---
+++ 30 Days Of Code/JS/13 Abstract Classes/Solution.js
@@ -0,0 +1,25 @@
+    // Declare your class here.
+    /**   
+    *   Class Constructor
+    *   
+    *   @param title The book's title.
+    *   @param author The book's author.
+    *   @param price The book's price.
+    **/
+    // Write your constructor here
+   class MyBook extends Book{
+      constructor(title, author,price) {
+          super(title, author);
+          this.price=price;
+      }
+    /**   
+    *   Method Name: display
+    *   
+    *   Print the title, author, and price in the specified format.
+    **/
+    // Write your method here
+   }
+// End class
+MyBook.prototype.display=function(){
+    console.log("Title: "+this.title+"\nAuthor: "+this.author+"\nPrice: "+this.price);
+}
