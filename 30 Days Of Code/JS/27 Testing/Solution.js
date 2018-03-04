d481eeb1be923afa16430f58c7827b5472bddb97
Mary Bond <mbond1224@users.noreply.github.com>
2018-03-04 06:15:29 -0800

    Create Solution.js
    Solution to the problem listed at https://www.hackerrank.com/challenges/30-testing/problem

---
+++ 30 Days Of Code/JS/27 Testing/Solution.js
@@ -0,0 +1,15 @@
+console.log('5')
+console.log('4 3')
+console.log('-1 -3 4 2')
+console.log('5 2')
+console.log('0 -1 2 1 4')
+//YES
+console.log('7 4')
+console.log('-1 0 1 1 2 3 4')
+
+//NO
+console.log('8 7')
+console.log('-1 0 0 0 0 0 0 1')
+//YES
+console.log('9 2')
+console.log('-1 0 0 0 0 0 1 3 4')
